6fad1497be3512137c422b884af1390c3045147d
kkamsogod <9310rlawlgns@gmail.com>
2024-10-16 19:53:44 +0900

    [feat]_Player, Paddle
    패들 아이템 관련 업데이트, 플레이어 1 2 구분지어서 목숨나눔에 따라 코드 변경, 아이템 적용 이후 버그 수정

--- Assets/Scripts/InGameScene/PaddleEffects/PaddleEffectHandler.cs
+++ Assets/Scripts/InGameScene/PaddleEffects/PaddleEffectHandler.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
-using static UnityEditor.Progress;
 
 public class PaddleEffectHandler : MonoBehaviour
 {
+    [SerializeField] private PlayerManager playerManager;
     private PaddleMovement paddleMovement;
     private PaddleSizeHandler paddleSizeHandler;
+    
+
+    private float speedEffectDuration = 0f;
+    private float sizeEffectDuration = 0f;
+    private float stopEffectDuration = 0f;
+
+    private float originalSpeedMultiplier = 1f;
+    private float originalSizeMultiplier = 1f;
+
+    public int playerID;
 
     private void Start()
     {
@@ -12,28 +22,96 @@ public class PaddleEffectHandler : MonoBehaviour
         paddleSizeHandler = GetComponent<PaddleSizeHandler>();
     }
 
-    // 이넘이 추가되면 복구 예정
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Item"))
-    //    {
-    //        Item item = other.GetComponent<Item>();
+    private void Update()
+    {
+        if (speedEffectDuration > 0)
+        {
+            speedEffectDuration -= Time.deltaTime;
+            if (speedEffectDuration <= 0f)
+            {
+                paddleMovement.AdjustPaddleSpeed(originalSpeedMultiplier);
+            }
+        }
 
-    //        if (item != null)
-    //        {
-    //            switch (item.itemType)
-    //            {
-    //                case ItemType.Speed:
-    //                    // 속도 조정
-    //                    paddleMovement.AdjustPaddleSpeed(2.0f);
-    //                    break;
+        if (sizeEffectDuration > 0)
+        {
+            sizeEffectDuration -= Time.deltaTime;
+            if (sizeEffectDuration <= 0f)
+            {
+                paddleSizeHandler.AdjustPaddleSize(originalSizeMultiplier);
+            }
+        }
 
-    //                case ItemType.Size:
-    //                    // 크기 조정
-    //                    paddleSizeHandler.AdjustPaddleSize(1.5f);
-    //                    break;
-    //            }
-    //        }
-    //    }
-    //}
+        if (stopEffectDuration > 0)
+        {
+            stopEffectDuration -= Time.deltaTime;
+            if (stopEffectDuration <= 0f)
+            {
+                paddleMovement.isStopped = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.CompareTag("Item"))
+        {
+            Item item = target.GetComponent<Item>();
+
+            if (item != null)
+            {
+                switch (item.itemType)
+                {
+                    case Item.Type.PaddleSpeedUp:
+                        paddleMovement.AdjustPaddleSpeed(2.0f);
+                        speedEffectDuration = 15.0f;
+                        Debug.Log("스피드업");
+                        break;
+
+                    case Item.Type.PaddleSpeedDown:
+                        paddleMovement.AdjustPaddleSpeed(0.5f);
+                        speedEffectDuration = 15.0f;
+                        Debug.Log("스피드다운");
+                        break;
+
+                    case Item.Type.PaddleSizeUp:
+                        paddleSizeHandler.AdjustPaddleSize(1.5f);
+                        sizeEffectDuration = 15.0f;
+                        Debug.Log("사이즈업");
+                        break;
+
+                    case Item.Type.PaddleSizeDown:
+                        paddleSizeHandler.AdjustPaddleSize(0.75f);
+                        sizeEffectDuration = 15.0f;
+                        Debug.Log("사이즈다운");
+                        break;
+
+                    case Item.Type.BallPowerUp:
+                        Debug.Log("구현안함");
+                        break;
+
+                    case Item.Type.BallSpeedUp:
+                        Debug.Log("구현안함");
+                        break;
+
+                    case Item.Type.BallTriple:
+                        Debug.Log("구현안함");
+                        break;
+
+                    case Item.Type.PaddleStopDebuff:
+                        paddleMovement.isStopped = true;
+                        stopEffectDuration = 3.0f;
+                        Debug.Log("멈춰");
+                        break;
+
+                    case Item.Type.BonusLife:
+                        playerManager.IncreasePlayerLife(playerID);
+                        Debug.Log("체력증가");
+                        break;
+
+                }
+                item.DestroyItem(true);
+            }
+        }
+    }
 }
