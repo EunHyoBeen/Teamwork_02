ab9a795582b846e4ba0736aede621d11e5f96efa
moomin <amily8050@gmail.com>
2024-10-21 17:41:32 +0900

    [feat] BoostItem.cs
    코드 수정, 부스팅아이템과 목숨 반투명 처리, 목숨 위치 조정

--- Assets/Scripts/InGameScene/Items/BoostItem.cs
+++ Assets/Scripts/InGameScene/Items/BoostItem.cs
@@ -1,75 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoostItem : MonoBehaviour
 {
-    [SerializeField] private GameObject itemPrefab;
+    // [SerializeField] private GameObject itemPrefab;
     [SerializeField] private ItemImages itemImages; 
-    public GameObject boostItem;
+    [SerializeField] private GameObject item1;
+    [SerializeField] private GameObject item2;
+    [SerializeField] private GameObject boostItem;
+
+    // public GameObject boostItem;
+    public Item.Type itemType;
 
     // Start is called before the first frame update
     void Start()
     {
         // Item.Type[] items = {};
-        Item.Type[] items = {Item.Type.BallPowerUp, Item.Type.BallTriple};
-        Debug.Log(items.Length);
+        Item.Type[] items = {Item.Type.PaddleSizeUp};
+        // Item.Type[] items = null;
+        // Debug.Log(items.Length);
         ShowBoostItem(items);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void ShowBoostItem(Item.Type[] items)
     {
-        float itemX = -1.2f;
-        float itemY = 4.55f;
 
-        // if (items.Length == 0 || items == null)
+        Image img1 = item1.GetComponent<Image>();
+        Image img2 = item2.GetComponent<Image>();
+
+        // if (items == null || items.Length == 0)
         // {
         //     boostItem.SetActive(false);
         // }
-        // else 
-        if (items.Length != 0)
+
+        switch(items.Length)
         {
-            boostItem.SetActive(true);
+            case 0:
+                boostItem.SetActive(false);
+                break;
+            case 1:
+                item1.SetActive(true);
+                img1.sprite = itemImages.List[(int)items[0]];
+                break;
 
-            for (int i = 0; i < items.Length; i++)
-            {
-                GameObject boostItem = Instantiate(itemPrefab, this.transform);
-
-                // 부모의 중심에 오브젝트 위치 설정
-                RectTransform rtf = boostItem.gameObject.GetComponent<RectTransform>();
-
-                if (rtf != null)
-                {
-                    rtf.anchoredPosition = new Vector2(0, 100);
-                }
-                rtf.localScale = new Vector3(0.25f, 0.25f, 1); 
-                
-                Item item = boostItem.GetComponent<Item>();
-
-                Rigidbody2D rb = boostItem.GetComponent<Rigidbody2D>();
-                rb.gravityScale = 0f;
-
-                itemX += 0.4f;
-                if (item != null)
-                {
-                    item.InitializeItem(itemX, itemY, items[i], Vector2.zero);
-                }
-
-                SpriteRenderer spriteRenderer = boostItem.GetComponent<SpriteRenderer>();
-                if (spriteRenderer != null && (int)items[i] < itemImages.List.Count)
-                {
-                    spriteRenderer.sprite = itemImages.List[(int)items[i]];
-                    spriteRenderer.sortingOrder = 2;
-                }
-            }
+            case 2:
+                item1.SetActive(true);
+                item2.SetActive(true);
+                img1.sprite = itemImages.List[(int)items[0]];
+                img2.sprite = itemImages.List[(int)items[1]];
+                break;
         }
-        
     }
 }
