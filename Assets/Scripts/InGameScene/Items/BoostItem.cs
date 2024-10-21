60de0b6b162957f480c22c4d1c8ae1f0258865c7
moomin <amily8050@gmail.com>
2024-10-21 12:04:03 +0900

    [fix] PlayerLife
    목숨 Canvas에 추가

--- Assets/Scripts/InGameScene/Items/BoostItem.cs
+++ Assets/Scripts/InGameScene/Items/BoostItem.cs
@@ -7,11 +7,14 @@ public class BoostItem : MonoBehaviour
 {
     [SerializeField] private GameObject itemPrefab;
     [SerializeField] private ItemImages itemImages; 
+    public GameObject boostItem;
 
     // Start is called before the first frame update
     void Start()
     {
-        Item.Type[] items = {Item.Type.BonusLife, Item.Type.BallPowerUp};
+        // Item.Type[] items = {};
+        Item.Type[] items = {Item.Type.BallPowerUp, Item.Type.BallTriple};
+        Debug.Log(items.Length);
         ShowBoostItem(items);
     }
 
@@ -23,31 +26,37 @@ public class BoostItem : MonoBehaviour
 
     public void ShowBoostItem(Item.Type[] items)
     {
-        float itemX = -2.5f;
-        float itemY = 4.2f;
+        float itemX = -1.2f;
+        float itemY = 4.55f;
 
+        // if (items.Length == 0 || items == null)
+        // {
+        //     boostItem.SetActive(false);
+        // }
+        // else 
         if (items.Length != 0)
         {
+            boostItem.SetActive(true);
+
             for (int i = 0; i < items.Length; i++)
             {
                 GameObject boostItem = Instantiate(itemPrefab, this.transform);
-                boostItem.layer = 5;
 
                 // 부모의 중심에 오브젝트 위치 설정
                 RectTransform rtf = boostItem.gameObject.GetComponent<RectTransform>();
 
                 if (rtf != null)
                 {
-                    rtf.anchoredPosition = new Vector2(0, 200);
+                    rtf.anchoredPosition = new Vector2(0, 100);
                 }
-                // rtf.localScale = Vector3.one; 
+                rtf.localScale = new Vector3(0.25f, 0.25f, 1); 
                 
                 Item item = boostItem.GetComponent<Item>();
 
                 Rigidbody2D rb = boostItem.GetComponent<Rigidbody2D>();
                 rb.gravityScale = 0f;
 
-                itemX += 0.5f;
+                itemX += 0.4f;
                 if (item != null)
                 {
                     item.InitializeItem(itemX, itemY, items[i], Vector2.zero);
